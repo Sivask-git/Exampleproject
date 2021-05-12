d1938b1a0e6d4f1201225b21b62282b3a9404c01
siva kumar <shivakumarfb17@gmail.com>
2021-05-12 11:12:36 +0530

    Initial commit

---
+++ src/com/company/Person.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Person {
+
+    public static void main(String[] args){
+        double value =3.22;
+        System.out.println("the value is"+value);
+    }
+
+}
